0fcc8839cb413b1662c6b9779481a8ceafbb2f3e
Rajiv Chaulagain <82865638+rajivchaulagain@users.noreply.github.com>
2023-05-27 23:05:10 +0545

    first commit

---
+++ data.js
@@ -0,0 +1,40 @@
+const data = {
+  courseType: [
+    {
+      id: 1,
+      title: "Full Time",
+    },
+    {
+      id: 2,
+      title: "Part Time",
+    },
+  ],
+  InterestArea: [
+    {
+      id: 1,
+      title: "agriculture , forestry , fisheries",
+    },
+    {
+      id: 2,
+      title: "Arts and humanities",
+    },
+    {
+      id: 3,
+      title: "Business and adminstration and law",
+    },
+  ],
+  Location: [
+    {
+      id: 1,
+      title: "Kathmandu",
+    },
+    {
+      id: 2,
+      title: "Koteshwor",
+    },
+    {
+      id: 3,
+      title: "Baneshwor",
+    },
+  ],
+};
