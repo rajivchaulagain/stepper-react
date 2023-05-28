912a1516c7cdf6a9c5b2980aca6ede964abd30e1
Rajiv Chaulagain <82865638+rajivchaulagain@users.noreply.github.com>
2023-05-28 20:26:35 +0545

    created steeper

--- new.js
+++ new.js
@@ -41,11 +41,10 @@ const data = {
       title: "Baneshwor",
     },
   ],
-  mockResults: [
+  mockResults : [
     {
       id: 1,
-      title:
-        "Athlone Community Training Center (CTC) - Catering and Hospitality",
+      title: "Athlone Community Training Center (CTC) - Catering and Hospitality",
       courseType: "Full-Time",
       location: "Kathmandu",
       duration: "2 Weeks",
@@ -83,7 +82,7 @@ const data = {
       duration: "4 Years",
       interestArea: "Engineering, Manufacturing, and Construction",
     },
-  ],
+  ]
 };
 
 const SectionTitle = ({ title, paragraph }) => {
@@ -95,45 +94,40 @@ const SectionTitle = ({ title, paragraph }) => {
   );
 };
 
-const SelectCourseCard = ({
-  title,
-  onClick,
-  isSelected,
-  id,
-  showCheckbox = true,
-}) => {
+const SelectCourseCard = ({ title, onClick, isSelected, id , showCheckbox=true }) => {
   console.log(title, id);
   return (
     <div className="card course-type" onClick={onClick}>
       <div className="card-body course-type-body">
         <div className="">
-        <i class="far fa-clock"></i>       
-          <p>
-            {title}
-            </p>
+          <i className="fa fa-clock" />
+          <br />
+          {title}
         </div>
-        <div style={{ textAlign: "right" }}>
+        <div style={{textAlign : 'right'}}>
           <button className={`rounded-button ${isSelected && "selected"}`}>{`${
             isSelected ? "Selected" : "Select"
           }`}</button>
-          {showCheckbox && (
-            <div>
-              <p>Choose your time slot</p>
-              {id === 1 ? (
-                <div>
-                  <input type="checkbox" disabled={!isSelected} checked />
-                  <label>Day-time</label>
-                </div>
-              ) : (
-                <div>
-                  <input type="checkbox" disabled={!isSelected} />
-                  <label style={{marginRight : '1rem'}}>Day-time</label>
-                  <input type="checkbox" disabled={!isSelected} />
-                  <label>Evening-time</label>
-                </div>
-              )}
-            </div>
-          )}
+          {
+            showCheckbox && (
+          <div>
+            <p>Choose your time slot</p>
+            {id === 1 ? (
+              <div>
+                <input type="checkbox" disabled={!isSelected} checked />
+                <label>Day-time</label>
+              </div>
+            ) : (
+              <div>
+                <input type="checkbox" disabled={!isSelected} />
+                <label>Day-time</label>
+                <input type="checkbox" disabled={!isSelected} />
+                <label>Evening-time</label>
+              </div>
+            )}
+          </div>
+            )
+          }
         </div>
       </div>
     </div>
@@ -141,119 +135,86 @@ const SelectCourseCard = ({
 };
 
 const CourseFormCard = (props) => {
+  const options = [
+    { value: 'option1', label: 'Option 1' },
+    { value: 'option2', label: 'Option 2' },
+    { value: 'option3', label: 'Option 3' },
+  ];
 
   return (
     <div className="course-form box-shadow">
       <div className="input-wrapper">
-        <input
-          type="text"
-          placeholder="Search course..."
-          value={props.inputSearch}
-          onChange={(e) => props.handleSearch(e.target.value)}
-        />
-        <span className="text-primary">Find Course</span>
+      <input type="text" placeholder="Search course..." value={props.inputSearch} onChange={(e) => props.handleSearch(e.target.value)} />
+      <span className="text-primary">Find Course</span>
       </div>
       <hr />
       <div>
         <label>Filters</label>
         <div className="select-wrapper">
           <select value={props.courseType.title}>
-            <option
-              disabled
-              selected
-              value=""
-              onChange={() =>
-                props.handleCourseTypeSelection(props.courseType.title)
-              }
-            >
-              Course Type
-            </option>
-            {data.courseType.map((type) => (
-              <option key={type.id} value={type.title}>
-                {type.title}
-              </option>
-            ))}
+          <option disabled selected value="" onChange={() => props.handleCourseTypeSelection(props.courseType.title)}>Course Type</option>
+          {data.courseType.map((type) => (<option key={type.id} value={type.title}>{type.title}</option>))}
           </select>
           <select>
-            <option disabled selected value="">
-              Location
-            </option>
-            {data.location.map((loc) => (
-              <option key={loc.id} value={loc.title}>
-                {loc.title}
-              </option>
-            ))}
+          <option disabled selected value="">Location</option>
+          {data.location.map((loc) => (<option key={loc.id} value={loc.title}>{loc.title}</option>))}
           </select>
-          <Select
-          className="select-interest"
-            isMulti
-            options={data.interestArea.map((area) => ({
-              value: area.id,
-              label: area.title,
-            }))}
-            defaultValue={props.interestArea.map((area) => ({
-              value: area.id,
-              label: area.title,
-            }))}           
-            styles={{
-              control: (baseStyles, state) => ({
-                ...baseStyles,
-                flex: 1,
-              }),
-            }}
+            <window.Select
+            options={options}
             // value={selectedOption}
             // onChange={handleChange}
           />
+          {/* <select>
+            <option disabled selected value="">Areas of Interest</option>
+            {data.interestArea.map((area) => (<option key={area.id} value={area.title}>{area.title}</option>))}
+          </select> */}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 const ShowingResults = (props) => {
-  const { searchVaue } = props;
+  const {searchVaue} = props
   return (
     <div className="showing-results">
       <div className="card-body box-shadow">
         <div>
-          <h3>
-            <span className="text-primary">TU869</span>
-            {searchVaue.title}
-          </h3>
+          <h3><span className="text-primary">TU869</span>{searchVaue.title}</h3>
           <hr />
         </div>
         <div>
           <ul>
             <li>
-              <i className="fa fa-clock-o"></i>
-              {searchVaue.courseType}
+            <i className="fa fa-clock"></i>
+            {searchVaue.courseType}
             </li>
             <li>
-              <i className="fa fa-map-marker"></i>
-              {searchVaue.location}
+            <i className="fa fa-map-marker"></i>
+           {searchVaue.location}
             </li>
             <li>
-              <i className="fa fa-clock"></i>
-              {searchVaue.duration}
+            <i className="fa fa-clock"></i>
+            {searchVaue.duration}
             </li>
             <li>
-              <i className="fa fa-hand"></i>
-              {searchVaue.interestArea}
+            <i className="fa fa-hand"></i>
+            {searchVaue.interestArea}
             </li>
           </ul>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 const Stepper = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [selectedCourseType, setSelectedCourseType] = useState(null);
   const [selectedInterestArea, setSelectedInterestArea] = useState([]);
   const [selectedLocation, setSelectedLocation] = useState(null);
-  const [inputSearch, setInputSearch] = useState("");
-  const [searchResults, setSearchResults] = useState(data.mockResults);
+  const [inputSearch , setInputSearch] = useState("");
+  const [searchResults , setSearchResults] = useState(data.mockResults);
 
   const handleNext = () => {
     setCurrentStep(currentStep + 1);
@@ -269,9 +230,7 @@ const Stepper = () => {
   };
   const handleInterestAreaSelection = (interestArea) => {
     if (selectedInterestArea.includes(interestArea)) {
-      setSelectedInterestArea(
-        selectedInterestArea.filter((area) => area !== interestArea)
-      );
+      setSelectedInterestArea(selectedInterestArea.filter((area) => area !== interestArea));
     } else {
       setSelectedInterestArea([...selectedInterestArea, interestArea]);
     }
@@ -282,8 +241,8 @@ const Stepper = () => {
   };
 
   const handleSearch = (value) => {
-    console.log("dada", { value });
-    setInputSearch(value);
+    console.log('dada' , {value});
+    setInputSearch(value)
     const filteredResults = data.mockResults.filter((result) => {
       return result.title.toLowerCase().includes(value.toLowerCase());
     });
@@ -303,20 +262,13 @@ const Stepper = () => {
       <div className="stepper-body">
         {currentStep === 1 && (
           <div className="step">
-            <div className="d-flex align-items-center">
+            <button disabled={!selectedCourseType} onClick={handleNext} style={{float : 'right'}}>
+              Next
+            </button>
             <SectionTitle
               title="Select course type"
               paragraph="We offer a wide range of full and part time courses cross Longford and Westmeath. Full time courses take place during the day and part time course are more flexible, courses are offered during the day, in the evening or online. Courses will be filtered according to your chosen option."
             />
-               <button
-              disabled={!selectedCourseType}
-              onClick={handleNext}
-              className="next-rounded-button"
-              // style={{ float: "right" }}
-            >
-              Next
-            </button>
-            </div>
             <div className="wrapper d-flex">
               {data.courseType.map((courseType) => (
                 <SelectCourseCard
@@ -333,31 +285,24 @@ const Stepper = () => {
 
         {currentStep === 2 && (
           <div className="step">
-            <button
-              disabled={selectedInterestArea.length === 0}
-              onClick={handleNext}
-              style={{ float: "right" }}
-              className="next-rounded-button"
-            >
-              Next
-            </button>
-            <button onClick={handlePrevious} style={{ float: "left" }} className="prev-rounded-button">
-              Previous
-            </button>
-            <SectionTitle
+              <button disabled={!selectedInterestArea} onClick={handleNext} style={{float : 'right'}}>
+                Next
+              </button>
+              <button onClick={handlePrevious} style={{float : 'left'}}>Previous</button>
+              <SectionTitle
               title="Interest Area"
               paragraph="We offer a wide range of full and part time courses cross Longford and Westmeath. Full time courses take place during the day and part time course are more flexible, courses are offered during the day, in the evening or online. Courses will be filtered according to your chosen option."
             />
             <div className="d-flex">
               {data.interestArea.map((interestArea) => (
                 <SelectCourseCard
-                  id={interestArea.id}
-                  title={interestArea.title}
-                  key={interestArea.id}
-                  showCheckbox={false}
-                  isSelected={selectedInterestArea.includes(interestArea)}
-                  onClick={() => handleInterestAreaSelection(interestArea)}
-                />
+                id={interestArea.id}
+                title={interestArea.title}
+                key={interestArea.id}
+                showCheckbox={false}
+                isSelected={selectedInterestArea.includes(interestArea)}
+                onClick={() => handleInterestAreaSelection(interestArea)}
+              />
               ))}
             </div>
           </div>
@@ -365,30 +310,30 @@ const Stepper = () => {
 
         {currentStep === 3 && (
           <div className="step step-3">
-            <button onClick={handlePrevious} style={{ float: "left" }} className="prev-rounded-button">
-              Previous
-            </button>
-            <SectionTitle
+            <button onClick={handlePrevious} style={{float : 'left'}}>Previous</button>
+                <SectionTitle
               title="Choose Course"
               paragraph="We offer a wide range of full and part time courses cross Longford and Westmeath. Full time courses take place during the day and part time course are more flexible, courses are offered during the day, in the evening or online. Courses will be filtered according to your chosen option."
             />
             <CourseFormCard
-              inputSearch={inputSearch}
-              handleSearch={handleSearch}
-              courseType={selectedCourseType}
-              handleCourseTypeSelection={handleCourseTypeSelection}
-              interestArea={selectedInterestArea}
-              handleInterestAreaSelection={handleInterestAreaSelection}
-              selectedLocation={selectedLocation}
-              handleLocationSelection={handleLocationSelection}
-            />
-            <div style={{ margin: "1rem 0" }}>
-              <h4 style={{ marginBottom: 0 }}>Showing results</h4>
+            inputSearch={inputSearch}
+            handleSearch={handleSearch}
+            courseType={selectedCourseType}
+            handleCourseTypeSelection={handleCourseTypeSelection}
+            interestArea={selectedInterestArea}
+            handleInterestAreaSelection={handleInterestAreaSelection}
+            selectedLocation={selectedLocation}
+            handleLocationSelection={handleLocationSelection}
+             />
+            <div style={{margin : '1rem 0'}}>
+              <h4 style={{marginBottom : 0}}>Showing results</h4>
               <span>{searchResults.length} courses</span>
             </div>
-            {searchResults.map((searchVaue) => (
-              <ShowingResults searchVaue={searchVaue} key={searchVaue.id} />
-            ))}
+            {
+              searchResults.map((searchVaue) => (
+                <ShowingResults searchVaue={searchVaue} key={searchVaue.id} />
+              ))
+            }
           </div>
         )}
       </div>
